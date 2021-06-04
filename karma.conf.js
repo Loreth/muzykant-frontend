8d29ece18de93cd1cb6709785840b340f7d68d96
Kamil Przeniosło <kamillo.prz@gmail.com>
2021-06-04 14:30:01 +0200

    Modify circleci config

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true,
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
+    singleRun: false,
     restartOnFileChange: true
   });
 };
